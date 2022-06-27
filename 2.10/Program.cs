1eb53932ec248f4e05972eeec5068e861a83ba0c
DoktorMandarin <faa1995alex@gmail.com>
2022-06-27 21:12:29 +0300

    Добавьте файлы проекта.

---
+++ 2.10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            var number = random.Next();
+            int x = 2;
+            var stepen = 0;
+            Console.WriteLine(number);
+
+           
+            for (var i = 0; ; i++)
+            {
+                if (number > Math.Pow(x, stepen))
+                {
+                    stepen = i;
+                                        
+                }
+                
+             Console.WriteLine($"{x} в степени {stepen} больше {number}");
+                
+            }
+             
+        }
+    }
+    
+}
